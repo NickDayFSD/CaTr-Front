0d5c28b3b94e9fc789a25503bebf0c2b22a3b898
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-10 11:38:31 -0700

    character list styling

--- src/components/characters/Character.jsx
+++ src/components/characters/Character.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './character.css';
 
-const Character = ({ characterName, level, description }) => (
-  <div>
+const Character = ({ characterName, level, description, backstory }) => (
+  <div className={styles.character}>
     <p>Name: {characterName}</p>
     <p>Level {level}</p>
     <p>{description}</p>
+    <p>{backstory}</p>
+    <button>edit</button>
   </div>
 );
 
@@ -13,6 +16,7 @@ Character.propTypes = {
   characterName: PropTypes.string,
   level: PropTypes.string,
   description: PropTypes.string,
+  backstory: PropTypes.string,
 };
 
 export default Character;
